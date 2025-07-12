34ad6141d615a448c4307bc8206cd0e7eb528eff
Vicgib <llumtivi@gmail.com>
2025-07-12 20:29:31 +0200

    Primer commit: proyecto Angular base

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Saludo } from './saludo/saludo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Saludo],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
-  protected title = 'MiPrimeraApp';
+export class AppComponent {
+  titulo: string = 'Mi Primera App en Angular';
 }
+
